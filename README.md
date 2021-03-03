4c3d06234b69d537a12478b984ee927bb8f4a075
leye195 <leye195@naver.com>
2021-03-04 00:05:41 +0900

    feat: radial Menu

--- README.md
+++ README.md
@@ -20,4 +20,5 @@ Drag and Drop, Form Warning, Slick 등등 라이브러리 없이 구현해보자
 - [x] Quiz
 - [x] Recipe App
 - [x] Drop Down
+- [ ] Radial Menu
 - [ ] Compass
---
+++ radialMenu/index.js
@@ -0,0 +1,45 @@
+const radial1 = document.querySelector('.radial-menu1 .toggle');
+const menu1 = document.querySelector('.radial-menu1 .menu');
+const plus1 = document.querySelector('.radial-menu1 #plus');
+
+const radial2 = document.querySelector('.radial-menu2 .toggle');
+const menu2 = document.querySelector('.radial-menu2 .menu');
+const plus2 = document.querySelector('.radial-menu2 #plus');
+
+let isOneClosed = true;
+let isTwoClosed = true;
+
+const handleToggle1 = () => {
+  if(isOneClosed) {
+    menu1.style.transform = `scale(3)`;
+    plus1.style.transform = 'rotate(45deg)';
+  } else {
+    menu1.style.transform = `scale(0)`;
+    plus1.style.transform = 'rotate(0deg)';
+  }
+  isOneClosed = !isOneClosed;
+}
+
+const handleToggle2 = () => {
+    if(isTwoClosed) {
+      menu2.style.transform = `scale(3)`;
+      menu2.style.opcity = 1;
+      plus2.style.transform = 'rotate(45deg)';
+      
+    } else {
+      menu2.style.transform = `scale(0)`;
+      menu2.style.opcity = 0;
+      plus2.style.transform = 'rotate(0deg)';
+    }
+    isTwoClosed = !isTwoClosed;
+  }
+
+
+
+
+const init = () => {
+  radial1.addEventListener('click',handleToggle1);
+  radial2.addEventListener('click',handleToggle2);
+}
+
+init();
