c233dffae07482f31213d8aae82d5fe4070cf2a6
leye195 <leye195@naver.com>
2023-09-08 22:44:19 +0900

    feat: apply scrollTrigger

--- gsap/practice1/index.js
+++ gsap/practice1/index.js
@@ -1,11 +1,14 @@
 (() => {
   gsap.registerPlugin(ScrollTrigger);
+  gsap.to(window, 0.7, {
+    scrollTo: 0,
+  });
 
   const tl = gsap.timeline();
   tl.to("body", {
     overflow: "hidden",
   });
-  tl.to(".box", {
+  tl.to(".box-container .box", {
     scale: 0.25,
     yoyo: true,
     backgroundColor: "white",
@@ -59,6 +62,68 @@
         })
         .to("body", {
           overflow: "auto",
+          onComplete: () => {
+            const tl2 = gsap.timeline({
+              scrollTrigger: {
+                trigger: ".section1",
+                toggleActions: "restart none reverse pause",
+                start: "top center",
+                end: "+=180",
+                scrub: 1,
+              },
+            });
+            tl2.to(".section1-card", {
+              opacity: 1,
+              scale: 0.5,
+            });
+            tl2.to(".section1-card .box", {
+              borderRadius: 50,
+              onComplete: () => {
+                const boxes = document.querySelectorAll(".section1-card .box");
+                console.log(boxes);
+                boxes.forEach((box, idx) => {
+                  gsap.to(box, {
+                    keyframes: [{ y: 10 }, { y: 0 }],
+                    ease: "power3",
+                    duration: 1.2,
+                    repeat: -1,
+                    delay: idx * 0.1,
+                  });
+                });
+              },
+            });
+
+            const section2 = document.querySelector(".section2");
+            const panels = gsap.utils.toArray(".section2 .panel");
+            gsap.to(panels, {
+              xPercent: -100 * (panels.length - 1),
+              ease: "none",
+              scrollTrigger: {
+                trigger: section2,
+                pin: true,
+                scrub: 0.5,
+                snap: 1 / (panels.length - 1),
+                end: () => "+=" + section2.offsetWidth,
+              },
+            });
+
+            const section1 = document.querySelector(".section1");
+            const wrapper = document.querySelectorAll(".wrapper");
+            wrapper.forEach((w, index) => {
+              gsap.fromTo(w, { opacity: 0 }, { opacity: 1 });
+              gsap.fromTo(
+                w,
+                { x: index % 2 === 0 ? w.scrollWidth : w.scrollWidth * -1 },
+                {
+                  x: 0,
+                  scrollTrigger: {
+                    trigger: section1,
+                    scrub: 0.25,
+                  },
+                }
+              );
+            });
+          },
         });
     },
   });
